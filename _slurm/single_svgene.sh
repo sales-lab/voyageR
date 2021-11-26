74cc2ac0063e2c48267669f14cff24a0c83eb4ab
davidecrs <corso.davide.92@gmail.com>
2021-11-26 16:42:47 +0100

    added script to run single benchmark svgenes

---
+++ _slurm/single_svgene.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name="svg_benchmark"
+#SBATCH --mail-type=ALL
+#SBATCH --time=96:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=50G
+#SBATCH --nodelist=xen5
+
+#
+# Copyright 2021 Davide Corso
+#
+
+
+
+package=$1
+dataset=$2
+
+mount_host=results/svgenes/${package}
+mount_container=/results/svgenes/${package}
+
+mkdir -p ${mount_host}
+
+singularity exec --bind ${mount_host}:${mount_container} singularity/voyager.simg bash /benchmark/benchmark_svgenes.sh -p ${package} -d ${dataset}
