5176a414757fa0fbb04d34bbe39322c4f98bbbbf
Dario <dario.righelli@gmail.com>
2022-01-28 11:47:48 +0100

    adding fdr threshold

--- _slurm/format_results.py
+++ _slurm/format_results.py
@@ -22,17 +22,17 @@ os.system("mkdir -p results/svgenes/spark_combined")
 os.system("mkdir -p results/svgenes/spatialde_combined")
 os.system("mkdir -p results/svgenes/stutility_combined")
 
-def write_combined_fdr(rawdf, colname_genes, colname_adjpval, package, method=None, log_pvalue=False):
+def write_combined_fdr(rawdf, colname_genes, colname_adjpval, package, method=None, log_pvalue=False, p_thr=1):
     df = rawdf.loc[:, [colname_genes, colname_adjpval]]
     if not log_pvalue:
         df.rename(columns={colname_genes: 'genes', colname_adjpval: 'adj_pvalue'}, inplace=True)
-            
+
         df.drop_duplicates(inplace=True)
 
         for i in df["adj_pvalue"]:
             assert isinstance(i, float)
 
-        x = df[df["adj_pvalue"] < 0.05]
+        x = df[df["adj_pvalue"] < p_thr]
         y = x.sort_values(by="adj_pvalue", ascending=True)
     else:
         df.rename(columns={colname_genes: 'genes', colname_adjpval: 'log_adj_pvalue'}, inplace=True)
@@ -40,61 +40,61 @@ def write_combined_fdr(rawdf, colname_genes, colname_adjpval, package, method=No
         for i in df["log_adj_pvalue"]:
             assert isinstance(i, float)
 
-        x = df[df["log_adj_pvalue"] < numpy.log10(0.05)]
+        x = df[df["log_adj_pvalue"] < numpy.log10(p_thr)]
         y = x.sort_values(by="log_adj_pvalue", ascending=True)
-    
+
     if method is None:
         filename =  f"results/svgenes/{package}_combined/voyager_fdr.csv"
     else:
         filename =  f"results/svgenes/{package}_combined/voyager_fdr_{method}.csv"
-    
+
     y.to_csv(filename, header=True, index=False, sep="\t")
 
 
-def write_combined_score(rawdf, colname_genes, colname_score, package, method=None):
+def write_combined_score(rawdf, colname_genes, colname_score, package, method=None, p_thr=1):
     df = rawdf.loc[:, [colname_genes, colname_score]]
-    
+
     df.rename(columns={colname_genes: 'genes', colname_score: 'score'}, inplace=True)
     df.drop_duplicates(inplace=True)
-    
+
     for i in df["score"]:
         assert isinstance(i, float)
-    
-    x = df[df["score"] < 0.05]
+
+    x = df[df["score"] < p_thr]
     y = x.sort_values(by="score", ascending=False)
     if method is None:
         filename =  f"results/svgenes/{package}_combined/voyager_score.csv"
     else:
         filename =  f"results/svgenes/{package}_combined/voyager_score_{method}.csv"
-    
+
     y.to_csv(filename, header=True, index=False, sep="\t")
 
 
 def write_combined_rank(rawdf, colname_genes, colname_rank, package, method=None):
     df = rawdf.loc[:, [colname_genes, colname_rank]]
-    
+
     df.rename(columns={colname_genes: 'genes', colname_rank: 'rank'}, inplace=True)
     df.drop_duplicates(inplace=True)
-    
+
     for i in df["rank"]:
         assert isinstance(i, int)
-    
+
     y = df.sort_values(by="rank", ascending=True)
     if method is None:
         filename =  f"results/svgenes/{package}_combined/voyager_rank.csv"
     else:
         filename =  f"results/svgenes/{package}_combined/voyager_rank_{method}.csv"
-    
+
     y.to_csv(filename, header=True, index=False, sep="\t")
 
 
 # boostgp = pd.read_table("./results/svgenes/boostgp/")
-
+fdr_thr = 1
 giotto1 = pd.read_table("results/svgenes/giotto/results_binSpect_kmeans.csv")
-write_combined_fdr(giotto1, colname_genes="genes", colname_adjpval="adj.p.value", package="giotto", method="binSpect_kmeans")
+write_combined_fdr(giotto1, colname_genes="genes", colname_adjpval="adj.p.value", package="giotto", method="binSpect_kmeans", p_thr=fdr_thr)
 
 giotto2 = pd.read_table("results/svgenes/giotto/results_binSpect_rank.csv")
-write_combined_fdr(giotto2, colname_genes="genes", colname_adjpval="adj.p.value", package="giotto", method="binSpect_rank")
+write_combined_fdr(giotto2, colname_genes="genes", colname_adjpval="adj.p.value", package="giotto", method="binSpect_rank", p_thr=fdr_thr)
 
 giotto3 = pd.read_table("results/svgenes/giotto/results_silhouette.csv")
 write_combined_score(giotto3, colname_genes="genes", colname_score="scores", package="giotto", method="silhouette")
@@ -102,8 +102,8 @@ write_combined_score(giotto3, colname_genes="genes", colname_score="scores", pac
 gliss = pd.read_table("results/svgenes/gliss/results.csv")
 fdrs = fdrcorrection([i for i in gliss.pvalue], alpha=0.01, method='indep', is_sorted=False)
 gliss["fdr"] = fdrs[1]
-gliss = gliss[gliss["reject"] == True]
-write_combined_fdr(gliss, colname_genes="geneID", colname_adjpval="fdr", package="gliss", method=None)
+#gliss = gliss[gliss["reject"] == True]
+write_combined_fdr(gliss, colname_genes="geneID", colname_adjpval="fdr", package="gliss", method=None, p_thr=fdr_thr)
 
 # gpcounts = pd.read_table("./results/svgenes/gpcounts/")
 
@@ -112,10 +112,10 @@ write_combined_fdr(gliss, colname_genes="geneID", colname_adjpval="fdr", package
 # meringue = pd.read_table("./results/svgenes/meringue/")
 
 raysel = pd.read_table("./results/svgenes/rayleighselection/results.csv")
-write_combined_fdr(raysel, colname_genes="Unnamed: 0", colname_adjpval="q0", package="rayleighselection", method=None)
+write_combined_fdr(raysel, colname_genes="Unnamed: 0", colname_adjpval="q0", package="rayleighselection", method=None, p_thr=fdr_thr)
 
 scgco = pd.read_table("results/svgenes/scgco/results.csv")
-write_combined_fdr(scgco, colname_genes="gene", colname_adjpval="fdr", package="scgco", method=None)
+write_combined_fdr(scgco, colname_genes="gene", colname_adjpval="fdr", package="scgco", method=None, p_thr=fdr_thr)
 
 seurat1 = pd.read_table("results/svgenes/seurat/results_markvariogram.csv")
 seurat1 = seurat1[seurat1["markvariogram.spatially.variable"] == True]
@@ -123,19 +123,19 @@ write_combined_rank(seurat1, colname_genes="Unnamed: 0", colname_rank="markvario
 
 seurat2 = pd.read_table("results/svgenes/seurat/results_moransi.csv")
 seurat2 = seurat2[seurat2["moransi.spatially.variable"] == True]
-write_combined_fdr(seurat2, colname_genes="Unnamed: 0", colname_adjpval="adj.pvalue", package="seurat", method="moransi")
+write_combined_fdr(seurat2, colname_genes="Unnamed: 0", colname_adjpval="adj.pvalue", package="seurat", method="moransi", p_thr=fdr_thr)
 
 sch = pd.read_table("results/svgenes/singlecellhaystack/results.csv")
-write_combined_fdr(sch, colname_genes="gene", colname_adjpval="log.p.adj", package="singlecellhaystack", method=None, log_pvalue=True)
+write_combined_fdr(sch, colname_genes="gene", colname_adjpval="log.p.adj", package="singlecellhaystack", method=None, log_pvalue=True, p_thr=fdr_thr)
 
 somde = pd.read_table("results/svgenes/somde/results.csv")
-write_combined_fdr(somde, colname_genes="g", colname_adjpval="qval", package="somde", method=None, log_pvalue=False)
+write_combined_fdr(somde, colname_genes="g", colname_adjpval="qval", package="somde", method=None, log_pvalue=False, p_thr=fdr_thr)
 
 spark1 = pd.read_table("results/svgenes/spark/results_spark.csv")
-write_combined_fdr(spark1, colname_genes="Unnamed: 0", colname_adjpval="adjusted_pvalue", package="spark", method="spark")
+write_combined_fdr(spark1, colname_genes="Unnamed: 0", colname_adjpval="adjusted_pvalue", package="spark", method="spark", p_thr=fdr_thr)
 
 spark2 = pd.read_table("results/svgenes/spark/results_sparkX.csv")
-write_combined_fdr(spark2, colname_genes="Unnamed: 0", colname_adjpval="adjustedPval", package="spark", method="sparkX")
+write_combined_fdr(spark2, colname_genes="Unnamed: 0", colname_adjpval="adjustedPval", package="spark", method="sparkX", p_thr=fdr_thr)
 
 spatialde = pd.read_table("results/svgenes/spatialde/results.csv")
-write_combined_fdr(spatialde, colname_genes="g", colname_adjpval="qval", package="spatialde", method=None)
+write_combined_fdr(spatialde, colname_genes="g", colname_adjpval="qval", package="spatialde", method=None, p_thr=fdr_thr)
