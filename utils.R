940df1921bc33234995885c15630d6a8878212e4
davidecrs <corso.davide.92@gmail.com>
2022-03-02 16:38:49 +0100

    Added libd_sample and rds file saving results

--- utils.R
+++ utils.R
@@ -14,14 +14,19 @@ detach_SpE <- function(spe) {
 }
 
 
-load_SpE <- function(dataset){
+load_SpE <- function(dataset, libd_sample){
     switch(dataset,
        spatialLIBD={
            spe <- spatialLIBD::fetch_data(type="spe")
+           libd_sample <- as.character(libd_sample)
+           stopifnot(libd_sample %in% c("151507", "151508", "151509", "151510", "151669", "151670", "151671", "151672", "151673", "151674", "151675", "151676"))
            ## randomly taking one out of 12 samples 
-           spe <- spe[, spe$sample_id == "151508"]
+           # SAMPLE Available: 151507, 151508, 151509, 151510, 151669, 151670, 151671, 151672, 151673, 151674, 151675, 151676, 
+           spe <- spe[, spe$sample_id == libd_sample]
        },
-       {##all other cases are TENxVisium dataset ids
+       {
+           ##all other cases are TENxVisium dataset ids
+           stopifnot(libd_sample == "null")
            # query for TENxVisium datasets
            eh <- suppressMessages(ExperimentHub())
            (q <- query(eh, "TENxVisium"))
@@ -40,11 +45,12 @@ write_results <- function(df, pckg_name, analysis, extra_name = ""){
     path_results <- paste0("/results/", analysis, "/", pckg_name)
     write.table(df, file = paste0(path_results, "/", "results", extra_name, ".csv"), quote = FALSE, sep="\t", 
             row.names = T, col.name = NA)
+    saveRDS(df, file = paste0(path_results, "/", "results", extra_name, ".rds"))
 }
 
 
-spe_to_h5ad <- function(dataset, filename){
-    spe <- load_SpE(dataset)
+spe_to_h5ad <- function(dataset, libd_sample, filename){
+    spe <- load_SpE(dataset, libd_sample)
     stopifnot(all(rownames(colData(spe)) == rownames(spatialCoords(spe))))
     colData(spe) <- cbind(colData(spe), spatialCoords(spe))
     colData(spe)$scaleFactor <- imgData(spe)$scaleFactor
