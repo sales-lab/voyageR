cf3189a321b12366a6066e702b732911ff916a57
davidecrs <corso.davide.92@gmail.com>
2022-03-07 17:28:59 +0100

    prepare_datasets with shuffle param

--- _slurm/prepare_datasets.sh
+++ _slurm/prepare_datasets.sh
@@ -2,11 +2,25 @@
 
 dataset=$1
 libd_sample=$2
+shuffle=$3
 
-dir=prepared_datasets
+if [[ $shuffle != "yes" && $shuffle != "no" ]]
+then
+    echo "Shuffle param wrong"
+    exit 1
+fi
 
-mkdir -p ${dir}
+base_folder="datasets_and_results"
 
-filename="${dir}/SpE_${dataset}_${libd_sample}.h5ad"
+if [ $shuffle == "yes" ]
+then
+   datasets_folder="${base_folder}/shuffled/${dataset}_${libd_sample}"
+   base_file="${datasets_folder}/spe_${dataset}_${libd_sample}_shuffled"
+else
+   datasets_folder="${base_folder}/normal/${dataset}_${libd_sample}"
+   base_file="${datasets_folder}/spe_${dataset}_${libd_sample}"
+fi
 
-singularity exec --writable-tmpfs singularity/voyager.sif Rscript -e "source('/benchmark/utils.R'); spe_to_h5ad('${dataset}', '${libd_sample}', '${filename}');"
+mkdir -p ${datasets_folder}
+
+singularity exec --writable-tmpfs singularity/voyager.sif Rscript -e "source('/benchmark/utils.R'); spe_to_files('${dataset}', '${libd_sample}', '${shuffle}', '${base_file}');"
