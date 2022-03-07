29bfbf22c0431bf6554d0abb32e4f48472dc15b6
davidecrs <corso.davide.92@gmail.com>
2022-03-07 17:33:08 +0100

    single_svgenes slurm with shuffle and check existing files

--- _slurm/single_svgene.sh
+++ _slurm/single_svgene.sh
@@ -14,29 +14,44 @@
 package=$1
 dataset=$2
 libd_sample=$3
+shuffle=$4
 
-echo "=== package: ${package} === dataset: ${dataset} === libd_sample: ${libd_sample} ==="
-
-if [ $dataset == "spatialLIBD" ]
+if [[ $shuffle != "yes" && $shuffle != "no" ]]
 then
-   dataset_name="${dataset}_${libd_sample}"
-else
-   dataset_name="${dataset}"
-fi
-
-mount_host=results_single_${dataset_name}/svgenes/${package}
-mount_container=/results/svgenes/${package}
-
-host_datasets=prepared_datasets
-container_dataset=/benchmark/datasets
-
-mkdir -p ${mount_host}
-
-checking_file="${host_datasets}/SpE_${dataset}_${libd_sample}.h5ad"
-
-if [ ! -f ${checking_file} ]; then
-    echo "${checking_file} file not found."
+    echo "Shuffle param wrong"
     exit 1
 fi
 
-singularity exec --writable-tmpfs --bind ${mount_host}:${mount_container} --bind ${host_datasets}:${container_dataset} singularity/voyager.sif python3 /benchmark/benchmark_svgenes.py -p ${package} -d ${dataset} -l ${libd_sample}
+echo "=== package: ${package} === dataset: ${dataset} === libd_sample: ${libd_sample} === shuffle: ${shuffle} ==="
+
+base_folder="datasets_and_results"
+
+if [ $shuffle == "yes" ]
+then
+   mount_host_datasets="${base_folder}/shuffled/${dataset}_${libd_sample}"
+   base_file="${mount_host_datasets}/spe_${dataset}_${libd_sample}_shuffled"
+else
+   mount_host_datasets="${base_folder}/normal/${dataset}_${libd_sample}"
+   base_file="${mount_host_datasets}/spe_${dataset}_${libd_sample}"
+fi
+
+mount_container_dataset=/benchmark/datasets
+
+checking_file_python="${base_file}.h5ad"
+if [ ! -f ${checking_file_python} ]; then
+    echo "${checking_file_python} python file not found."
+    exit 1
+fi
+
+checking_file_r="${base_file}.rds"
+if [ ! -f ${checking_file_r} ]; then
+    echo "${checking_file_r} R file not found."
+    exit 1
+fi
+
+mount_host_results="${mount_host_datasets}/results_single"
+mount_container_results=/results/svgenes/${package}
+
+mkdir -p ${mount_host_results}
+
+singularity exec --writable-tmpfs --bind ${mount_host_results}:${mount_container_results} --bind ${mount_host_datasets}:${mount_container_dataset} singularity/voyager.sif python3 /benchmark/benchmark_svgenes.py -p ${package} -d ${dataset} -l ${libd_sample} -s ${shuffle}
