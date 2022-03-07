cef55201fb6be44a19d889c68e408484f4a52313
davidecrs <corso.davide.92@gmail.com>
2022-03-07 17:25:57 +0100

    SpatialDE: anndata loaded from h5ad file

--- svgenes/spatialde/benchmark.py
+++ svgenes/spatialde/benchmark.py
@@ -35,9 +35,8 @@ def spatialde_workflow(counts, coords):
 
 
 args = parse_args()
-dataset = args.dataset
-libd_sample = args.libd_sample
-ad = load_anndata(dataset, libd_sample)
+base_file = args.base_file
+ad = load_anndata(base_file)
 
 counts, coords, _ = detach_anndata(ad, symbols=False, transpose_counts=True, coord_on_colnames=False)
 spatialde_workflow(counts, coords)
