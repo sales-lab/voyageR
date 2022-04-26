97b03177afe2da89bfc45c1debfa6b1df78b3db2
davidecrs <corso.davide.92@gmail.com>
2022-04-26 13:08:52 +0200

    Added new script to create spatialLIBD samples

---
+++ _slurm/create_spatialLIBD_samples.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# rm datasets_and_results/**/spatialLIBD_*.rds
+# rm datasets_and_results/**/spatialLIBD_*.h5ad
+
+
+while read dts; do
+    IFS=_ read libd_sample shuffle filter_genes_ncounts filter_genes_pcspots <<< ${dts}
+    echo "spatialLIBD ${libd_sample} ${shuffle} ${filter_genes_ncounts} ${filter_genes_pcspots}"
+    #./prepare_datasets.sh spatialLIBD ${libd_sample} ${shuffle} ${filter_genes_ncounts} ${filter_genes_pcspots} ; 
+done < specific_spatialLIBD/spatialLIBD_samples_filters.txt
