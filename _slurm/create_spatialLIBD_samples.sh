6c172985cd6b3f3501a1e215f87ddaa62b349a07
davidecrs <corso.davide.92@gmail.com>
2022-05-03 14:38:14 +0200

    prepare spatialLIBD dataset

--- _slurm/create_spatialLIBD_samples.sh
+++ _slurm/create_spatialLIBD_samples.sh
@@ -7,5 +7,5 @@
 while read dts; do
     IFS=_ read libd_sample shuffle filter_genes_ncounts filter_genes_pcspots <<< ${dts}
     echo "spatialLIBD ${libd_sample} ${shuffle} ${filter_genes_ncounts} ${filter_genes_pcspots}"
-    #./prepare_datasets.sh spatialLIBD ${libd_sample} ${shuffle} ${filter_genes_ncounts} ${filter_genes_pcspots} ; 
+    ./prepare_datasets.sh spatialLIBD ${libd_sample} ${shuffle} ${filter_genes_ncounts} ${filter_genes_pcspots} ; 
 done < specific_spatialLIBD/spatialLIBD_samples_filters.txt
