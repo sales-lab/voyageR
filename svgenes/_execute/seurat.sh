b7c86fccec61c9fccca8ec9aef273729dedf26b6
davidecrs <corso.davide.92@gmail.com>
2022-06-01 15:12:04 +0200

    removed markvariogram script

--- svgenes/_execute/seurat.sh
+++ svgenes/_execute/seurat.sh
@@ -9,5 +9,5 @@ base_file=$1
 
 echo "======= Seurat ======="
 # Benchmark Seurat
-/usr/bin/time -v -o ./bench_markvariogram.txt Rscript /benchmark/svgenes/seurat/benchmark_markvariogram.R ${base_file} &> ./stdoe_markvariogram.txt
+# /usr/bin/time -v -o ./bench_markvariogram.txt Rscript /benchmark/svgenes/seurat/benchmark_markvariogram.R ${base_file} &> ./stdoe_markvariogram.txt
 /usr/bin/time -v -o ./bench_moransi.txt Rscript /benchmark/svgenes/seurat/benchmark_moransi.R ${base_file} &> ./stdoe_moransi.txt
